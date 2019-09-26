ec603038e35a67a2b7d82a7ea7950e769e8fd7b7
christian williams <christian.williamsj@gmail.com>
2019-09-26 02:18:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,5 +4,5 @@ def create_project_hash
   html = file.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
-  kickstarter.css(li.)
+  kickstarter.css(li.project.grid_4)
 end
