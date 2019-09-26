c64505d0afe5f3cb606da41855d100f02d164275
christian williams <christian.williamsj@gmail.com>
2019-09-26 02:21:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,12 +1,11 @@
-
 require "nokogiri"
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
+  html = file.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
+    title = project.css("h2.bbcard_name strong a").text 
     projects[title] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
@@ -15,4 +14,5 @@ def create_project_hash
     }
   end
   projects
+end
 end
